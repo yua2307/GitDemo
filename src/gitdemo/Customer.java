bd69fdb98bcb3af6cb9f63ba12ed083ba995329d
yua2307 <54582695+yua2307@users.noreply.github.com>
2020-09-24 19:08:37 +0700

    Create Customer.java

---
+++ src/gitdemo/Customer.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitdemo;
+
+/**
+ *
+ * @author macbookpro
+ */
+public class Customer {
+    int id;
+    String nameCustomer;
+}
